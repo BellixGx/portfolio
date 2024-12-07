9a00fb71fee859dbe4bc23e2ee4fee7249acdf6e
BellixGx <150390289+BellixGx@users.noreply.github.com>
2024-12-07 13:51:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -329,7 +329,7 @@ const skillss = [
                     <path d="M361.4,394.74H150.6a5.38,5.38,0,0,0,0,10.76H361.4a5.38,5.38,0,0,0,0-10.76Z"/>
                   </svg>
                   <span className="text">
-                    <a href="https://drive.google.com/file/d/1yW5DujAnhHq6YXp99offCthECOv0Q-Pv/view?usp=drive_link" target="_blank" rel="noopener noreferrer" >
+                    <a href="https://drive.google.com/file/d/1gBMp4cVZH09uI3uR0Cu6XeAHMXyPq_zf/view?usp=drive_link" target="_blank" rel="noopener noreferrer" >
                       resume
                     </a>
                   </span>
